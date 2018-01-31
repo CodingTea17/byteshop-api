7d55e9af765529cbeaad160d296b8aa5ccd9a72a
David Hale <david.hale7@gmail.com>
2018-01-31 15:58:38 -0800

    account model spec and validations

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Account, 'validations' do
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :user_id }
+  it { should validate_length_of(:name).is_at_least(2).is_at_most(20) }
 end
